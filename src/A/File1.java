4ec018fc3a318972c946cdec1d003d85dfe258f7
Priyvrat Chaturvedi <priyvrat.chaturvedi11@gmail.com>
2023-05-23 08:11:06 +0530

    A1 branc commit

--- src/A/File1.java
+++ src/A/File1.java
@@ -1,8 +1,11 @@
 package A;
 
 public class File1 {
-public static void main(String[]args) {
+	
+	public static void main(String[]args) {
 		
-		System.out.println("In branch A");
+		System.out.println("In branch A1");
+		System.out.println("In branch NewA1");
+		System.out.println("In branch OldA1");
 	}
 }
