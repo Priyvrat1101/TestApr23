de01f0cfbbe52c43075c2e8be81d20c377db3acd
Priyvrat Chaturvedi <priyvrat.chaturvedi11@gmail.com>
2023-05-23 08:53:56 +0530

    Changes in B2

--- src/A/File1.java
+++ src/A/File1.java
@@ -3,6 +3,8 @@ package A;
 public class File1 {
 public static void main(String[]args) {
 		
-		System.out.println("In branch A");
+		System.out.println("In branch B2");
+		System.out.println("In branch InB2");
+		System.out.println("In branch OutB2");
 	}
 }
