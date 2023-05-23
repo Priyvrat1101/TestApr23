641363f4f2af0a72fd78fc6303bed68615f2354d
Priyvrat Chaturvedi <priyvrat.chaturvedi11@gmail.com>
2023-05-23 08:49:17 +0530

    Chanegs in A2

--- src/A/File1.java
+++ src/A/File1.java
@@ -3,6 +3,8 @@ package A;
 public class File1 {
 public static void main(String[]args) {
 		
-		System.out.println("In branch A");
+		System.out.println("In branch A2");
+		System.out.println("In branch InA2");
+		System.out.println("In branch OutA2");
 	}
 }
